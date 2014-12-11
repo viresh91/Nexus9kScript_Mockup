e8d78f434d6097b15950ddbddc3e341a39368800
Gopalasivam <gopal@onecloudinc.com>
2014-12-11 18:08:16 +0530

    Added Inintial Mockup folders

---
+++ Config-Mgmt/automate_FEX_configuration.py
@@ -0,0 +1,21 @@
+"""
+Nexus:9000:9516:NX-OS Release 6.2
+Category:Configuration Management
+Title:FEX Configuration
+Short Description:This script is to automate configuring verification.
+Long Description:This script is to automate configuring FEX and verification.
+Input:FEX ID
+Output:Config Result
+"""
+
+import os
+
+def main():
+    """
+    This method will do the config automation functionality 
+    """
+    print "Printing Result"
+
+
+if __name__ == '__main__':
+    main()
---
+++ OS-Mgmt/automate_softwate_patch.py
@@ -0,0 +1,21 @@
+"""
+Nexus:9000:9516:NX-OS Release 6.2
+Category:OS/Patch Management
+Title:Automate Vlan creation
+Short Description:This script is to automate patch.
+Long Description:This script is to automate software patch management.
+Input:Patch ID
+Output:Result
+"""
+
+import os
+
+def main():
+    """
+    This method will do the config automation functionality 
+    """
+    print "Printing Result"
+
+
+if __name__ == '__main__':
+    main()
