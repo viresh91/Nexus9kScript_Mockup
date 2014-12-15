513315325373d81950d2c281047de97058084910
Gopalasivam <gopal@onecloudinc.com>
2014-12-15 17:40:37 +0530

    Added New Script Files for all the Categories.

---
+++ OS-Mgmt/add_or_remove_softwate_patch.py
@@ -0,0 +1,21 @@
+"""
+Nexus:9000:9516:NX-OS Release 6.2
+Category:OS/Patch Management
+Title:Add/Remove Software Patches
+Short Description:This script is to Add/Remove Software Patches.
+Long Description:This script is to add or remove software patches.
+Input:Patch ID
+Output:Result
+"""
+
+import os
+
+def main():
+    """
+    This method will do the config automation functionality 
+    """
+    print "Printing Result"
+
+
+if __name__ == '__main__':
+    main()
---
+++ OS-Mgmt/automate_softwate_upgrade_process.py
@@ -0,0 +1,21 @@
+"""
+Nexus:9000:9516:NX-OS Release 6.2
+Category:OS/Patch Management
+Title:Automate Software Upgrade Process
+Short Description:This script is to automate upgrade process.
+Long Description:This script is to automate software upgrade process.
+Input:Process ID
+Output:Result
+"""
+
+import os
+
+def main():
+    """
+    This method will do the config automation functionality 
+    """
+    print "Printing Result"
+
+
+if __name__ == '__main__':
+    main()
