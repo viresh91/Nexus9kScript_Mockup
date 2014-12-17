f4bcb04863b0a469c7946b5e5814f6bc689ab625
Gopalasivam <gopal@onecloudinc.com>
2014-12-17 23:53:26 +0530

    Update OSPF_Neighbor_adj_flap_issues.py

--- Troubleshooting/OSPF_Neighbor_adj_flap_issues.py
+++ Troubleshooting/OSPF_Neighbor_adj_flap_issues.py
@@ -1,11 +1,11 @@
-"""
-Nexus:9000:9504:NX-OS Release 6.2
-Category:Troubleshooting Tools
-Title:Fix for OSPF Neighbor adj flap issues
-Short Description:This script is to fix OSPF issues.
-Long Description:This script is to fix OSPF <neighbor> adjacency relationship changes.
-Input:Target ID
-Output:Result
+"""Script Cataloging Information
+:Product Info:Nexus::9000::9504::NX-OS Release 6.2
+:Category:Troubleshooting
+:Title:Fix for OSPF Neighbor adj flap issues
+:Short Description:This script is to fix OSPF issues.
+:Long Description:This script is to fix OSPF <neighbor> adjacency relationship changes.
+:Input:Target ID
+:Output:Result
 """
 
 import os
