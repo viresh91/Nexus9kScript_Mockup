98c46d26d60267bb0463fcd1f587426926eb6ffb
Gopalasivam <gopal@onecloudinc.com>
2014-12-17 20:03:34 +0530

    Modified critical syslog file

--- Monitoring/monitor_critical_syslog.py
+++ Monitoring/monitor_critical_syslog.py
@@ -1,11 +1,13 @@
-"""
-Nexus:9000:9516:NX-OS Release 6.2
-Category:Monitoring
-Title:Critical Syslog Monitoring
-Short Description:This script to monitor Critical Syslog Errors.
-Long Description:This Script to monitor monitor critical Syslog errors like Alerting and take action. Example: ( ospf adj flapping more than X times in Y duration, cost out the link).
-Input:Syslog ID
-Output:Critical Syslog Error Status.
+"""Script Cataloging Information
+:Product Info:Nexus::9000::9516::NX-OS Release 6.2
+:Category:Monitoring
+:Title:Critical Syslog Monitoring
+:Short Description:This script to monitor Critical Syslog Errors.
+:Long Description:This Script to monitor monitor critical Syslog errors like 
+Alerting and take action. 
+Example: ( ospf adj flapping more than X times in Y duration, cost out the link).
+:Input:Syslog ID
+:Output:Critical Syslog Error Status.
 """
 
 import os
