069c22784e418618a7ed0b7caef66e47e11eac0a
Gopalasivam <gopal@onecloudinc.com>
2014-12-19 01:38:03 +0530

    Added Sample script

---
+++ Troubleshooting/samplescript.py
@@ -0,0 +1,21 @@
+"""
+Nexus:9000:9504:NX-OS Release 6.2
+Category:Troubleshooting Tools
+Title:Health Check
+Short Description:This script is to check Health for the switch.
+Long Description:This script is to check Health for the switch like Entire switch diagnostics.
+Input:Target ID
+Output:Result
+"""
+
+import os
+
+def main():
+    """
+    This method will do the fix functionality for the issues 
+    """
+    print "Printing Result"
+
+
+if __name__ == '__main__':
+    main()
