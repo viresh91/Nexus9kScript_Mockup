2ddf8555a2441b88da4976d3d7b0bb3938149bc8
Gopalasivam <gopal@onecloudinc.com>
2015-01-07 04:06:36 +0530

    Sample script added to nexusscripts folder

---
+++ nexusscripts/OSPF_Neighbor_adj_flap_issues.py
@@ -0,0 +1,21 @@
+"""Script Cataloging Information
+:Product Info:Nexus::9000::9504::NX-OS Release 6.2
+:Category:Troubleshooting
+:Title:Fix for OSPF Neighbor adj flap issues
+:Short Description:This script is to fix OSPF issues.
+:Long Description:This script is to fix OSPF <neighbor> adjacency relationship changes.
+:Input:Target ID
+:Output:Result
+"""
+
+import os
+
+def main():
+    """
+    This method will do the fix functionality for the issues 
+    """
+    print "Printing Result"
+
+
+if __name__ == '__main__':
+    main()
