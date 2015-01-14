24cf0b52571b7f2947febfef02c34e4f9fffcd23
Gopalasivam <gopal@onecloudinc.com>
2015-01-14 10:54:22 +0530

    Addded sample for troublesshhoting category for realtime testing

---
+++ nexusscripts/Sample_FEX_issues.py
@@ -0,0 +1,21 @@
+"""Script Cataloging Information
+:Product Info:Nexus::9000::9516::NX-OS Release 6.2
+:Category:Troubleshooting
+:Title:Sample Fix for FEX issues
+:Short Description:This script is to fix FEX issues.
+:Long Description:This script is to fix FEX changes.
+:Input:Target ID
+:Output:Result
+"""
+
+import os
+
+def main():
+    """
+    This method will do the fix functionality for the issues 
+    """
+    print "Printing Result"
+
+
+if __name__ == '__main__':
+    main()
