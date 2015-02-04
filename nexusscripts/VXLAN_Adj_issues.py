9cd93c5ca72d03d51455acf37a38ad06fb138334
Gopalasivam <gopal@onecloudinc.com>
2015-02-05 00:10:48 +0530

    Uploaded New Scripts

---
+++ nexusscripts/VXLAN_Adj_issues.py
@@ -0,0 +1,21 @@
+"""
+Nexus:9000:9504:NX-OS Release 6.2
+Category:Troubleshooting Tools
+Title:VXLAN Adj issues
+Short Description:This script is to fix VXLAN issues.
+Long Description:This script is to fix VXLAN Adj Relationship issues.
+Input:Target ID
+Output:Result
+"""
+
+import os
+
+def main():
+    """
+    This method will do the fix functionality for the issues 
+    """
+    print "Printing Result"
+
+
+if __name__ == '__main__':
+    main()
---
+++ nexusscripts/add_or_remove_softwate_patch.py
@@ -0,0 +1,21 @@
+"""Script Cataloging Information
+:Product Info:Nexus::9000::9516::NX-OS Release 6.2
+:Category:OS-Mgmt
+:Title:Add/Remove Software Patches
+:Short Description:This script is to Add/Remove Software Patches.
+:Long Description:This script is to add or remove software patches.
+:Input:Patch ID
+:Output:Result
+"""
+
+import os
+
+def main():
+    """
+    This method will do the config automation functionality 
+    """
+    print "Printing Result"
+
+
+if __name__ == '__main__':
+    main()
