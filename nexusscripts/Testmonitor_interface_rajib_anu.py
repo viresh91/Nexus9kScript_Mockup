f391f556f9948da517adf46d88b9d16c616665eb
Gopalasivam <gopal@onecloudinc.com>
2015-03-11 17:30:06 +0530

    Added test script

---
+++ nexusscripts/Testmonitor_interface_rajib_anu.py
@@ -0,0 +1,22 @@
+"""Script Cataloging Information
+:Product Info:Nexus::9000::9516::NX-OS Release 6.2
+:Category:Monitoring
+:Title:Test Interface Monitoring demo for Rajib & Anurag
+:Short Description:This script is to monitor Interface counters.
+:Long Description:This script is to monitor Interface counters like
+Drops, Errors, Utilization.
+:Input:Interface ID
+:Output:Drop, Errors and Utilization Status.
+"""
+
+import os
+
+def main():
+    """
+    This method will do the monitoring functionality 
+    """
+    print "Printing Status"
+
+
+if __name__ == '__main__':
+    main()
