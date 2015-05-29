924a96e0cbfea43eae86532df431ee9bdef31ff9
Gopalasivam <gopal@onecloudinc.com>
2015-05-29 15:32:53 +0530

    Added script with empty header

---
+++ nexusscripts/today_emptyheader_test_system_gopal.py
@@ -0,0 +1,22 @@
+"""Script Cataloging Information
+:Product Info:Nexus::9000::9516::NX-OS Release 6.2
+:Category:Monitoring
+:Box Type:On-Box
+:Title:
+:Short Description:
+:Long Description:
+:Input:
+:Output:
+"""
+
+import os
+
+def main():
+    """
+    This method will do the monitoring functionality 
+    """
+    print "Printing Status"
+
+
+if __name__ == '__main__':
+    main()
