9fd005cc5a9134282996483a71e82b62c1505fa2
Gopalasivam <gopal@onecloudinc.com>
2015-05-29 15:02:32 +0530

    modified today my test script

--- nexusscripts/today_test_system_gopal.py
+++ nexusscripts/today_test_system_gopal.py
@@ -2,11 +2,11 @@
 :Product Info:Nexus::9000::9516::NX-OS Release 6.2
 :Category:Monitoring
 :Box Type:On-Box
-:Title:Today Test Script with by Gopal
+:Title:Today Test Script with modification by Gopal
 :Short Description:This script to monitor System resources and modified.
-:Long Description:This Script to monitor System resources like 
+:Long Description:This Script to monitor System resources and modified like 
 CPU, Memory, Buffers.
-:Input:System IDs
+:Input:System ID
 :Output:CPU, Memory, Buffers Status 
 """
 
