67a55aef2a002ae66a09c3502093ef415717b443
Gopalasivam <gopal@onecloudinc.com>
2015-05-29 14:56:14 +0530

    today my test script

---
+++ nexusscripts/today_test_system_gopal.py
@@ -0,0 +1,23 @@
+"""Script Cataloging Information
+:Product Info:Nexus::9000::9516::NX-OS Release 6.2
+:Category:Monitoring
+:Box Type:On-Box
+:Title:Today Test Script with by Gopal
+:Short Description:This script to monitor System resources and modified.
+:Long Description:This Script to monitor System resources like 
+CPU, Memory, Buffers.
+:Input:System IDs
+:Output:CPU, Memory, Buffers Status 
+"""
+
+import os
+
+def main():
+    """
+    This method will do the monitoring functionality 
+    """
+    print "Printing Status"
+
+
+if __name__ == '__main__':
+    main()
